193d6355f39ddf316c3ed9833a29b4af7f30cbac
HongMyungKuk <skdfw1212@naver.com>
2023-01-07 11:28:19 +0900

    decimal to binary function test

---
+++ math/changebinary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void decimaltobinary(int n){
+
+	printf("%d : MyFunc\n", n);
+}
+
+int main(){
+
+	int n;
+	printf("input number: ");
+	scanf("%d", &n);
+	
+	decimaltobinary(n);
+
+	return 0;
+}
